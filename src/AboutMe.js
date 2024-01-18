5f8cdbcb446e702027b91d7b975e052348c57c7e
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-01-18 17:49:03 -0500

    making about me

---
+++ src/AboutMe.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class AboutMe extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About Me</h1>
+        <div>
+          <p>SAMPLE TEXT</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default AboutMe
