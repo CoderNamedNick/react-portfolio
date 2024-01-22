a70604c4237a5c1a3ee1db413a137f742c998a1c
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-01-22 17:43:02 -0500

    finished fist project css

--- src/Projects.js
+++ src/Projects.js
@@ -1,19 +1,22 @@
 import React from "react";
 import Project1 from './Screenshot 2024-01-22 172206.png'
+import "./Projects.css"
 
 class Projects extends React.Component {
   render () {
     return (
-      <div>
+      <div className="PRJ-main-div">
         <div className="First-PRJ">
-          <img className="Project-Pics" src={Project1} alt="Hidive Project"></img>
-          <h2>Hidive Clone</h2>
-          <h3>LINK</h3>
-          <p>
-            This is My favorite project! I wanted to do something Not only 
-            Original.. As in NO tutorials how to do it, but i wanted to make something big with 
-            Pure vanilla Javascript. this by far took th longest but im most proud of it!
-          </p>
+          <img className="Project-Pics" src={Project1} alt=""></img>
+          <div className="PRJ-descr-div">
+            <h1>Hidive Clone</h1>
+            <h3>LINK</h3>
+            <p className="p-descr">
+              This is My favorite project! I wanted to do something not only 
+              Original.. As in NO tutorials how to do it, but i wanted to make something big with 
+              Pure vanilla Javascript. this by far took th longest but im most proud of it!
+            </p>
+          </div>
         </div>
         <div className="Second-PRJ">
           <img src="" alt="Hidive Project"></img>
