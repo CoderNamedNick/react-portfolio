96c3ba6c970327e3437c7ee1c8b2d257370bba24
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-01-23 15:50:04 -0500

    deleted header adjusted all

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Header from './Header';
 import Intro from './Intro';
 import AboutMe from './AboutMe';
 import PreviousJobs from './PreviousJobs';
@@ -9,7 +8,6 @@ import Footer from './Footer';
 function App() {
   return (
     <div className="App">
-        <Header />
           <Intro />
           <AboutMe />
           <PreviousJobs />
