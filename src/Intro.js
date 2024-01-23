f48df9ee42a0bcac1e9d73fc7f167508ea8ac6e3
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-01-23 16:05:58 -0500

    made some css ganna add logos

--- src/Intro.js
+++ src/Intro.js
@@ -6,16 +6,22 @@ import Urabe from "./Urabe.png"
 class Intro extends React.Component{
   render() {
     return (
-      <div id="intro" className="Intro-main-Div color-change-2x">
-        <div>
-          <img className="heartbeat profile-Pic" src={Urabe} alt="HandsomeMan"></img>
+      <div>
+        <div id="intro" className="Intro-main-Div color-change-2x">
+          <div>
+            <img className="heartbeat profile-Pic" src={Urabe} alt="HandsomeMan"></img>
+          </div>
+          <div className="Div2">
+            <h1>Nicholas Lloyd</h1>
+            <h1>Junior Web Developer</h1>
+            <h2>Niles,MI</h2>
+            <a className="GithubLink" href="https://github.com/CoderNamedNick"><h5>GITHUB</h5></a>
+            <a><h5>My Favorite Project</h5></a>
+          </div>
         </div>
-        <div className="Div2">
-          <h1>Nicholas Lloyd</h1>
-          <h1>Junior Web Developer</h1>
-          <h2>Niles,MI</h2>
-          <a className="GithubLink" href="https://github.com/CoderNamedNick"><h5>GITHUB</h5></a>
-          <a><h5>My Favorite Project</h5></a>
+        <div className="Div3">
+          <h1>Skills</h1>
+          {/* make logos and other Skills here */}
         </div>
       </div>
     )
